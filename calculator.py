685fbb00ba5eb8a5a0522729863387242f114af7
raj13-cyber <rajm13121999@gmail.com>
2024-07-01 19:25:37 +0530

    first commit of the day

--- calculator.py
+++ calculator.py
@@ -3,45 +3,8 @@ from tkinter import *
 import tkinter as tk
 from tkinter import ttk
 
-# basic window of calculator
-calculator = tk.Tk()
 
 
-# calculator name and size
-calculator.title("Raj calculator") # title of the calculator
-calculator.geometry("550x550+20+20")
-
-calculator.attributes('-topmost', 1)
-
-# changing the icon
-img = PhotoImage(file='/home/rajendiran/codes/logo.png')
-calculator.iconphoto(False,img)
-
-# creating label
-label_1 = Label(calculator, text = 'please click', fg="white", bg= "green").pack()
-
-ttk.Label(calculator, text = 'raj').pack()
-
-def button_clicked():
-    print('rajendiran')
-
-button = ttk.Button(calculator, text='more fun', command=button_clicked)
-button.pack()
-
-
-
-
-
-
-
-
-def select(option):
-    print(option)
-
-
-ttk.Button(calculator, text='Rock', command=lambda: select('Rock')).pack()
-ttk.Button(calculator, text='Paper',command=lambda: select('Paper')).pack()
-ttk.Button(calculator, text='Scissors', command=lambda: select('Scissors')).pack()
 
 
 
