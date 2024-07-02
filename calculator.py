d2a13ec203ec625f389270a75504a7bef4e781c9
raj13-cyber <rajm13121999@gmail.com>
2024-07-02 21:58:29 +0530

    todays commit

--- calculator.py
+++ calculator.py
@@ -5,8 +5,21 @@ from tkinter import ttk
 
 
 
+calculator = tk.Tk()
+
+mybutton = Button(calculator, text= "1", fg= 'white', bg= 'green')
+mybutton.pack()
 
 
+Entry_box= Entry(calculator)
+Entry_box.pack()
+
+Entry_box.insert(0,"enter the proper value")
+
+Frame = ttk.Frame(calculator, padding=10)
+Frame.pack()
+
+ttk.Button(Frame, text="quit", command=calculator.destroy).grid(column=0,row=0)
 
 calculator.mainloop()
 
