87dd4ef094256d755c15f71065762ca17d4f0a6b
raj13-cyber <rajm13121999@gmail.com>
2024-07-10 18:04:54 +0530

    final commit

--- sign.py
+++ sign.py
@@ -21,46 +21,32 @@ def login_clicked():
         title = "information",
         message = msg
     )
-
 #signin frame
 signin = ttk.Frame(login)
 signin.pack(padx=10, pady=10, fill="x",expand=True)
 
+
 # email
 
-email_label = ttk.Label( signin, text= "Emai Address:")
+email_label = ttk.Label( signin, text= "Email Address:")
 email_label.pack(fill="x", expand= True)
 
 email_entry = ttk.Entry( signin, textvariable = email)
 email_entry.pack(fill='x', expand=True)
 
+email_entry.focus()
+
 #password
 password_label = ttk.Label(signin, text = "password:")
 password_label.pack(fill='x', expand=TRUE)
 
-password_Entry = ttk.Entry(signin, textvariable=password)
+password_Entry = ttk.Entry(signin, textvariable=password,)
 password_Entry.pack(fill="x", expand=True)
 
 # login button
 
 login_button = ttk.Button(signin, text="Login", command=login_clicked)
 login_button.pack(fill='x', pady=15)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
