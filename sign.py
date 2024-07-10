e6062b72a3ec17dbdb96771be379d0173e64ad05
raj13-cyber <rajm13121999@gmail.com>
2024-07-10 17:07:10 +0530

    first commit

---
+++ sign.py
@@ -0,0 +1,9 @@
+from tkinter import*
+import tkinter as tk 
+from tkinter import ttk 
+from tkinter.messagebox  import showinfo
+
+login = tk.Tk()
+login.geometry("250x250")
+login.resizable(False, False)
+login.title("sigin In")
