35d36d5357ea0bad2676b5882ecbc1055c9b78e2
raj13-cyber <rajm13121999@gmail.com>
2024-07-13 17:15:48 +0530

    final commit

---
+++ login.py
@@ -0,0 +1,43 @@
+# import tkinter module and Tk themed widgets(old and new)
+from tkinter import*
+import tkinter as tk
+from tkinter import ttk
+from tkinter.messagebox  import showinfo
+
+
+login_form = tk.Tk()
+login_form.title("Login")
+login_form.geometry("300x250")
+
+
+fields = {}
+
+#store email and password 
+email= tk.StringVar()
+password=tk.StringVar()
+
+
+def login_clicked():
+    msg= f'you entered {email.get()} and password {password.get()}'
+
+    showinfo(
+        title = "Login Info",
+        message = msg
+    )
+
+
+# create user name label 
+fields['username_label'] = ttk.Label(text="User Name:")
+fields['username'] = ttk.Entry()
+
+# create password label 
+fields['password_label'] = ttk.Label(text = "Password:") 
+fields['password'] = ttk.Entry()
+
+# loop
+for field in fields.values():
+    field.pack(anchor=tk.W, padx=10, pady=5, fill=tk.X)
+
+ttk.Button(text="Login", command=login_clicked).pack(anchor=tk.W,padx=10, pady=5)
+
+login_form.mainloop()
